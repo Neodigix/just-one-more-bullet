c6205284009285f2d079127c54be134d6dcd9e5f
Arek <arkadiusz.czader@neodigix.com>
2026-06-04 12:48:31 +0200

    Update square enemy

--- enemy.js
+++ enemy.js
@@ -54,7 +54,7 @@ class Enemy {
     }
   }
   dropItem() {
-    if (getRandomInt(0, 20) == 0) {
+    if (getRandomInt(0, 12) == 0) {
       const newItem = new Item([this.x, this.y]);
       gameVars.newItems.push(newItem);
     }
@@ -81,7 +81,7 @@ class SolidEnemy extends Enemy{
   }
   draw(ctx) {
     const pos = convertPosToCanvas(this.x, this.y);
-    ctx.fillStyle = colors.solidEnemy;
+    ctx.fillStyle = colors.enemy;
     ctx.beginPath();
     ctx.arc(
       pos[0],  // x
@@ -91,6 +91,16 @@ class SolidEnemy extends Enemy{
       2 * Math.PI  // ending angle
     )
     ctx.fill();
+    ctx.beginPath();
+    ctx.strokeStyle = colors.solidEnemy;
+    ctx.arc(
+      pos[0],  // x
+      pos[1],  // y
+      20*gameVars.scale,  // radius
+      0,  // starting angle
+      2 * Math.PI  // ending angle
+    )
+    ctx.stroke();
   }
   
   hitByBullet(bullet) {
@@ -113,11 +123,11 @@ class SolidEnemy extends Enemy{
 class SquareEnemy extends Enemy{
   constructor(x, y, speed) {
     super(x, y, speed)
-    this.danger = 5;
+    this.danger = 2;
   }
   draw(ctx) {
     const pos = convertPosToCanvas(this.x, this.y);
-    ctx.fillStyle = colors.solidEnemy;
+    ctx.fillStyle = colors.enemy;
     ctx.beginPath();
     ctx.rect(
       pos[0] - (25*gameVars.scale),  // x
@@ -126,27 +136,6 @@ class SquareEnemy extends Enemy{
       45*gameVars.scale
     )
     ctx.fill();
-  }
-  
-  hitByBullet(bullet) {
-    this.isAlive = false;
-    const newEnemy = new Enemy(this.x, this.y, 40, this.id);
-    enemiesToAdd.push(newEnemy);
-    
-    // Reflect bullet
-    const prevBulletX = bullet.previousX;
-    const prevBulletY = bullet.previousY;
-    const left = this.x - 20;
-    const right = this.x + 20;
-    const top = this.y - 20;
-    const bottom = this.y + 20;
-    if (prevBulletX < left || prevBulletX > right)
-      bullet.direction[0] = -bullet.direction[0]
-    if (prevBulletY < top || prevBulletY > bottom)
-      bullet.direction[1] = -bullet.direction[1]
-    bullet.sleepIterations = 10;
-    bullet.lastHitEnemyId = this.id;
-    bullet.bounce();
   }
   
   isInside(x, y) {
@@ -163,5 +152,54 @@ class SquareEnemy extends Enemy{
     if (y > bottom)
       return false;
     return true;
+  }
+}
+
+class SolidSquareEnemy extends SquareEnemy{
+  constructor(x, y, speed) {
+    super(x, y, speed)
+    this.danger = 5;
+  }
+  draw(ctx) {
+    const pos = convertPosToCanvas(this.x, this.y);
+    ctx.fillStyle = colors.enemy;
+    ctx.beginPath();
+    ctx.rect(
+      pos[0] - (25*gameVars.scale),  // x
+      pos[1] - (25*gameVars.scale),  // y
+      45*gameVars.scale,
+      45*gameVars.scale
+    )
+    ctx.fill();
+    ctx.beginPath();
+    ctx.strokeStyle = colors.solidEnemy;
+    ctx.rect(
+      pos[0] - (20*gameVars.scale),  // x
+      pos[1] - (20*gameVars.scale),  // y
+      35*gameVars.scale,
+      35*gameVars.scale
+    )
+    ctx.stroke();
+  }
+  
+  hitByBullet(bullet) {
+    this.isAlive = false;
+    const newEnemy = new SquareEnemy(this.x, this.y, 40, this.id);
+    enemiesToAdd.push(newEnemy);
+    
+    // Reflect bullet
+    const prevBulletX = bullet.previousX;
+    const prevBulletY = bullet.previousY;
+    const left = this.x - 20;
+    const right = this.x + 20;
+    const top = this.y - 20;
+    const bottom = this.y + 20;
+    if (prevBulletX < left || prevBulletX > right)
+      bullet.direction[0] = -bullet.direction[0]
+    if (prevBulletY < top || prevBulletY > bottom)
+      bullet.direction[1] = -bullet.direction[1]
+    bullet.sleepIterations = 3;
+    bullet.lastHitEnemyId = this.id;
+    bullet.bounce();
   }
 }
